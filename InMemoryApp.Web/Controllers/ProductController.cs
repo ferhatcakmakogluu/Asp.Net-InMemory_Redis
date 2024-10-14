9a99c12392dde895382ca4e05633e3e84f6f0d2c
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-10-14 16:22:40 +0300

    TryGetValue, GetOrCreate and Remove functions

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -13,12 +13,27 @@ namespace InMemoryApp.Web.Controllers
 
         public IActionResult Index()
         {
-            _memoryCache.Set<string>("zaman",DateTime.Now.ToString());
+            /*
+                memory de "zaman" adinda data var mi kontrol ediyoruz.
+                TryGetValue ile kontrol et, eger zaman yoksa zamanCache olarak olustur
+             */
+            
+            if(!_memoryCache.TryGetValue("zaman", out string zamanCache))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
             return View();
         }
 
         public IActionResult Show()
         {
+            //_memoryCache.Remove("zaman");
+
+            //zaman'i almaya calis, yoksa olustur (datetime.now ile)
+            _memoryCache.GetOrCreate("zaman", option =>
+            {
+                return DateTime.Now.ToString();
+            });
             ViewBag.zaman = _memoryCache.Get<string>("zaman");
             return View();
         }
