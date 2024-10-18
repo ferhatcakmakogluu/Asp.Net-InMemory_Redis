0b00fa1a8020aaba3529b58a717ea59fa5c81211
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-10-18 23:17:16 +0300

    RedisExchange service

---
+++ RedisExchangeApi.Web/Services/RedisService.cs
@@ -0,0 +1,31 @@
+﻿using StackExchange.Redis;
+
+namespace RedisExchangeApi.Web.Services
+{
+    public class RedisService 
+    {
+        private readonly string _redisHost;
+        private readonly string _redisPort;
+        private readonly IConfiguration _configuration;
+        public IDatabase _database { get; set; }
+        private ConnectionMultiplexer _redis;
+
+        public RedisService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _redisHost = _configuration["Redis:Host"];
+            _redisPort = _configuration["Redis:Port"];
+        }
+
+        public void Connect()
+        {
+            var configString = $"{_redisHost}:{_redisPort}";
+            _redis = ConnectionMultiplexer.Connect(configString);
+        }
+
+        public IDatabase GetDb(int db)
+        {
+            return _redis.GetDatabase(db);
+        }
+    }
+}
