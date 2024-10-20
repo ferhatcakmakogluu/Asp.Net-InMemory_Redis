5e85f80b55fc87963a91ed7b973e6db62d6da003
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-10-20 13:36:42 +0300

    RedisExchange Hash Type

---
+++ RedisExchangeApi.Web/Controllers/HashTypeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeApi.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeApi.Web.Controllers
+{
+    public class HashTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase _db;
+        private string HashTypeKey = "hashDictionary";
+
+        public HashTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            _db = _redisService.GetDb(4);
+        }
+        public IActionResult Index()
+        {
+            Dictionary<string,string> dic = new Dictionary<string,string>();
+            if (_db.KeyExists(HashTypeKey))
+            {
+                _db.HashGetAll(HashTypeKey).ToList().ForEach(x =>
+                {
+                    dic.Add(x.Name, x.Value);
+                });
+            }
+            return View(dic);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string key, string value)
+        {
+            _db.HashSet(HashTypeKey, key, value);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteItem(string key)
+        {
+            _db.HashDelete(HashTypeKey, key);
+            return RedirectToAction("Index");
+        }
+    }
+}
